b333f6d89eac633aa1cb99f4d0b4553ddf84564c
Steven Nguyen <nguyenbaotrung.hcm@gmail.com>
2022-11-14 23:25:05 +0700

    Update project

--- src/app/squid-game/squid-game.component.ts
+++ src/app/squid-game/squid-game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Player } from './player/player';
 import { SquidGameService } from './squid-game.service';
@@ -11,14 +11,29 @@ import { SquidGameService } from './squid-game.service';
 export class SquidGameComponent implements OnInit {
   public players: Observable<any>;
   public selectedPlayers: number[] = [];
+  private elm: HTMLElement;
 
-  constructor(private squidGameService: SquidGameService) {
+  constructor(
+    private squidGameService: SquidGameService,
+    private elmRef: ElementRef
+  ) {
     this.players = squidGameService.players;
+    this.elm = elmRef.nativeElement;
   }
 
   ngOnInit(): void {}
 
-  onPlayerSelect(player: Player) {
+  onPlayerSelect(player: Player, $event: MouseEvent) {
+    console.log($event.clientX, $event.clientY);
+
+    const dynamite = document.createElement('img');
+    dynamite.className = 'dynamite';
+    dynamite.src = '/assets/img/dynamite_cursor.png';
+    dynamite.style.position = 'fixed';
+    dynamite.style.top = $event.clientY + 'px';
+    dynamite.style.left = $event.clientX + 'px';
+    this.elm.appendChild(dynamite);
+
     if (!this.squidGameService.isPLayerAlive(player)) return;
     player.selected = !!player.selected ? false : true;
   }
@@ -27,6 +42,16 @@ export class SquidGameComponent implements OnInit {
     if (!this.selectedPlayers.length) return;
 
     this.squidGameService.kill(this.selectedPlayers);
+  }
+
+  boom() {
+    const selectedElements = this.elm.getElementsByClassName('selected');
+    const selectedElementsLength = selectedElements.length;
+    const boom = document.createElement('div');
+    boom.className = 'boom';
+    for (let i = 0; i < selectedElementsLength; i++) {
+      selectedElements.item(i)?.appendChild(boom);
+    }
   }
 
   private setSelected(player: Player) {
