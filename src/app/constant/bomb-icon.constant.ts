691b026f0310311012af99a9871349b2de265117
Steven Nguyen <nguyenbaotrung.hcm@gmail.com>
2022-11-16 00:04:30 +0700

    Update code

---
+++ src/app/constant/bomb-icon.constant.ts
@@ -0,0 +1,7 @@
+import { environment } from "src/environments/environment";
+
+export const ExplosionIcons = [
+  environment.baseHref + '/assets/img/bomb.png',
+  environment.baseHref + '/assets/img/cylinder.png',
+  environment.baseHref + '/assets/img/dynamite.png',
+]
