170c3e12303190da0068bd8f8642fe3a7619d662
Steven Nguyen <nguyenbaotrung.hcm@gmail.com>
2022-11-21 14:19:32 +0700

    animation for admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -4,11 +4,13 @@ import { AppService } from '../app.service';
 import { BaseComponent } from '../base.component';
 import { Player } from '../interface/player.interface';
 import { PlayerStatus } from '../constant/player-status.constant';
+import { fadeAnimation } from '../animation/fade.animation';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
+  animations: [fadeAnimation]
 })
 export class AdminComponent extends BaseComponent implements OnInit {
   public allPlayers$!: Observable<Player[]>;
