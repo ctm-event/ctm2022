e5deefb1f72c304cd49bb4fc7b5628defd9a9fa6
Steven Nguyen <steven.nguyen@contemi.com>
2022-11-24 17:53:07 +0700

    Hehehe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { fadeAnimation } from '../animation/fade.animation';
+import { AppHelper } from '../app.helper';
+import { AppService } from '../app.service';
+import { Player } from '../interface/player.interface';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,16 @@ import { fadeAnimation } from '../animation/fade.animation';
   styleUrls: ['./home.component.scss'],
   animations: [fadeAnimation]
 })
-export class HomeComponent {
-  constructor() {}
+export class HomeComponent implements OnInit{
+  players$!: Observable<Player[]>;
+
+  constructor(private appService: AppService, private helper: AppHelper) {}
+
+  ngOnInit(): void {
+    this.players$ = this.appService.allActivePlayers;
+  }
+
+  getAvatar(player: Player) {
+    return this.helper.getPlayerAvatar(player);
+  }
 }
